c0a0359d5966023227b2a1a5c522e252539585dd
Soc-pr <audrius.priveda@gmail.com>
2023-04-27 19:22:49 +0300

    daugmaž veikia po pakeitimų :D

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('autoservice', include('autoservice.urls')),
+    path('', RedirectView.as_view(url='autoservice/', permanent=True)),
+    path('autoservice/', include('autoservice.urls')),
 ]
